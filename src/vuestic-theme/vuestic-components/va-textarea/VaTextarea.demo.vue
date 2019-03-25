9308d0e1324a57d60604f33fb22cb7040dc32dd0
Alexander Boriskin <alexander.boriskin12@gmail.com>
2019-03-25 13:37:04 +0300

    feat(va-textarea): update textarea demo

---
+++ src/vuestic-theme/vuestic-components/va-textarea/VaTextarea.demo.vue
@@ -0,0 +1,95 @@
+<template>
+  <VbDemo>
+    <VbContainer title="Simple Textarea">
+      <va-textarea
+        v-model="empty"
+      />
+    </VbContainer>
+    <VbContainer title="Textarea With Placeholder">
+      <va-textarea
+        v-model="empty"
+        placeholder="Name"
+      />
+    </VbContainer>
+    <VbContainer title="Textarea With Defined Rows">
+      <va-textarea
+        v-model="empty"
+        :rows="7"
+      />
+    </VbContainer>
+    <VbContainer title="Textarea With Label">
+      <va-textarea
+        v-model="text"
+        label="Name"
+      />
+    </VbContainer>
+    <VbContainer title="Textarea With Message">
+      <va-textarea
+        v-model="text"
+        :messages="messages"/>
+    </VbContainer>
+    <VbContainer title="Disabled Textarea">
+      <va-textarea
+        v-model="text"
+        disabled
+      />
+    </VbContainer>
+    <VbContainer title="Readonly Textarea">
+      <va-textarea
+        v-model="text"
+        readonly
+      />
+    </VbContainer>
+    <VbContainer title="Removable Textarea">
+      <va-textarea
+        v-model="text"
+        removable
+      >
+      </va-textarea>
+    </VbContainer>
+    <VbContainer title="Textarea With Error">
+      <va-textarea
+        v-model="text"
+        label="Name"
+        error
+      />
+    </VbContainer>
+    <VbContainer title="Textarea With Error">
+      <va-textarea
+        v-model="text"
+        label="Name"
+        success
+      />
+    </VbContainer>
+    <VbContainer title="Textarea With Error Message">
+      <va-textarea
+        v-model="text"
+        label="Name"
+        error
+        :error-messages="errorMessages"/>
+    </VbContainer>
+  </VbDemo>
+</template>
+
+<script>
+import VaTextarea from './VaTextarea'
+import VaButton from './../va-button/VaButton'
+import VaIcon from './../va-icon/VaIcon'
+
+export default {
+  components: {
+    VaTextarea,
+    VaButton,
+    VaIcon
+  },
+  data () {
+    return {
+      empty: '',
+      text: 'The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through.',
+      phone: '33 310-86-24',
+      messages: ['Required field'],
+      errorMessages: ['Detailed error message']
+    }
+  }
+}
+</script>
---
+++ src/vuestic-theme/vuestic-components/va-textarea/VaTextarea.vue
@@ -0,0 +1,109 @@
+<template>
+  <va-input-wrapper
+    class="va-textarea"
+    :class="{ 'va-input-wrapper--focused': isFocused }"
+    :disabled="disabled"
+    :error="error"
+    :success="success"
+    :messages="messages"
+    :error-messages="errorMessages"
+  >
+    <div
+      class="va-textarea__slot d-flex">
+      <label
+        :style="labelStyles"
+        aria-hidden="true"
+        class="va-textarea__slot__label title"
+      >
+        {{ label }}
+      </label>
+      <textarea
+        class="py-1 px-2"
+        :rows="rows"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        :readonly="readonly"
+        v-model="currentValue"
+        @input="$emit('input', currentValue)"
+        @focus="updateFocusState(true)"
+        @blur="updateFocusState(false)"
+      />
+    </div>
+    <va-icon
+      @click.native="clearContent()"
+      v-if="removable"
+      slot="append"
+      class="pointer pb-1"
+      :color="error ? 'danger': ''"
+      :style="{ color: '#babfc2'}"
+      icon="ion ion-md-close ion"
+    />
+  </va-input-wrapper>
+</template>
+
+<script>
+import VaInputWrapper from '../va-input/VaInputWrapper'
+import VaInput from '../va-input/VaInput'
+
+export default {
+  name: 'va-textarea',
+  extends: VaInput,
+  components: {
+    VaInputWrapper
+  },
+  props: {
+    rows: {
+      type: Number,
+      default: 5
+    }
+  },
+  data () {
+    return {
+      currentValue: this.value,
+      isFocused: false
+    }
+  },
+  computed: {
+    labelStyles () {
+      return {
+        color: this.error ? this.$themes.danger : ''
+      }
+    }
+  },
+  methods: {
+    clearContent () {
+      if (this.removable) {
+        this.$emit('input', '')
+      }
+    },
+    updateFocusState (isFocused) {
+      this.isFocused = isFocused
+    },
+  },
+}
+</script>
+
+<style lang='scss'>
+  .va-textarea {
+
+    &__slot {
+      position: relative;
+
+      &__label {
+        position: absolute;
+        top: -0.25rem;
+        left: 0.5rem;
+      }
+
+      textarea {
+        background-color: transparent;
+        border-style: none;
+        outline: none;
+
+        &::placeholder {
+          color: $brand-secondary;
+        }
+      }
+    }
+  }
+</style>
